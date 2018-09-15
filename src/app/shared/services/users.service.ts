6faf0db82825f10a85e08b8a268c3f5f469315b9
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-15 15:52:08 -0400

    Added userService

---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user.model';
+import { BaseApi } from '../core/base-api';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class UsersService extends BaseApi {
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  getUserByEmail(email: string): Observable<User> {
+    return this.get(`users?email=${email}`).pipe(
+      map((users: User[]) => users[0] ? users[0] : undefined)
+    );
+  }
+
+  createNewUser(user: User): Observable<User> {
+    return this.post('users', user);
+  }
+}
