3666abcbbb5848d7098f5dcec6de5d201b8dbc18
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-17 17:25:41 -0400

    Check email bounce time

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { UsersService } from '../../shared/services/users.service';
