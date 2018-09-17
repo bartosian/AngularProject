cfd98d47c4ce0739175f83cfd4ff2c90033615ce
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-17 11:23:34 -0400

    Fixed interceptor bug

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -26,6 +26,7 @@ export class BillPageComponent implements OnInit, OnDestroy {
       this.billService.getBill(),
       this.billService.getCurrency()
     ).subscribe((data: [Bill, any]) => {
+      console.log(data);
       this.bill = data[0];
       this.currency = data[1];
       this.isLoaded = true;
